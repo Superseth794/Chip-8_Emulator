80230ef2e630361cc28c28b4e364f2358a723319
Hugo Majerczyk <Superseth794@gmail.com>
2020-01-22 16:28:24 +0100

    Add C++ project

---
+++ Chip-8_Emulator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Chip-8_Emulator
+//
+//  Created on 22/01/2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
