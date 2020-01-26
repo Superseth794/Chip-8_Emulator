37229d618528181e35bb34461101b3ef5b42b51c
Hugo Majerczyk <Superseth794@gmail.com>
2020-01-26 02:58:17 +0100

    Added class to associate each possible key with its name

---
+++ Chip-8_Emulator/InputNames.hpp
@@ -0,0 +1,147 @@
+//
+//  InputNames.h
+//  Chip-8_Emulator
+//
+//  Created by Jaraxus on 25/01/2020.
+//
+
+#ifndef InputNames_h
+#define InputNames_h
+
+# include "ExternalLibrairies/TSL/hopscotch_map.h"
+
+# include <SFML/System.hpp>
+
+namespace chp {
+
+class ExtendedInputs {
+public:
+    
+    static std::string getKeyName(sf::Keyboard::Key const& key) {
+        for (auto it {keys.begin()}; it != keys.end(); ++it) {
+            if (it->second == key)
+                return it->first;
+        }
+        return "";
+    }
+    
+    static sf::Keyboard::Key getAssociatedKey(std::string const& name) {
+        if (keys.contains(name))
+            return keys.at(name);
+        else
+            return sf::Keyboard::Unknown;
+    }
+    
+    static tsl::hopscotch_map<std::string, sf::Keyboard::Key> keys;
+};
+
+inline tsl::hopscotch_map<std::string, sf::Keyboard::Key> ExtendedInputs::keys {
+    {"Unknown", sf::Keyboard::Unknown},
+    {"A", sf::Keyboard::A},
+    {"B", sf::Keyboard::B},
+    {"C", sf::Keyboard::C},
+    {"D", sf::Keyboard::D},
+    {"E", sf::Keyboard::E},
+    {"F", sf::Keyboard::F},
+    {"G", sf::Keyboard::G},
+    {"H", sf::Keyboard::H},
+    {"I", sf::Keyboard::I},
+    {"J", sf::Keyboard::J},
+    {"K", sf::Keyboard::K},
+    {"L", sf::Keyboard::L},
+    {"M", sf::Keyboard::M},
+    {"N", sf::Keyboard::N},
+    {"O", sf::Keyboard::O},
+    {"P", sf::Keyboard::P},
+    {"Q", sf::Keyboard::Q},
+    {"R", sf::Keyboard::R},
+    {"S", sf::Keyboard::S},
+    {"T", sf::Keyboard::T},
+    {"U", sf::Keyboard::U},
+    {"V", sf::Keyboard::V},
+    {"W", sf::Keyboard::W},
+    {"X", sf::Keyboard::X},
+    {"Y", sf::Keyboard::Y},
+    {"Z", sf::Keyboard::Z},
+    {"Z", sf::Keyboard::Z},
+    {"Num0", sf::Keyboard::Num0},
+    {"Num1", sf::Keyboard::Num1},
+    {"Num2", sf::Keyboard::Num2},
+    {"Num3", sf::Keyboard::Num3},
+    {"Num4", sf::Keyboard::Num4},
+    {"Num5", sf::Keyboard::Num5},
+    {"Num6", sf::Keyboard::Num6},
+    {"Num7", sf::Keyboard::Num7},
+    {"Num8", sf::Keyboard::Num8},
+    {"Num9", sf::Keyboard::Num9},
+    {"Escape", sf::Keyboard::Escape},
+    {"LControl", sf::Keyboard::LControl},
+    {"LShift", sf::Keyboard::LShift},
+    {"LAlt", sf::Keyboard::LAlt},
+    {"LSystem", sf::Keyboard::LSystem},
+    {"RControl", sf::Keyboard::RControl},
+    {"RShift", sf::Keyboard::RShift},
+    {"RAlt", sf::Keyboard::RAlt},
+    {"RSystem", sf::Keyboard::RSystem},
+    {"Menu", sf::Keyboard::Menu},
+    {"LBracket", sf::Keyboard::LBracket},
+    {"RBracket", sf::Keyboard::RBracket},
+    {"Semicolon", sf::Keyboard::Semicolon},
+    {"Comma", sf::Keyboard::Comma},
+    {"Period", sf::Keyboard::Period},
+    {"Quote", sf::Keyboard::Quote},
+    {"Slash", sf::Keyboard::Slash},
+    {"Backslash", sf::Keyboard::Backslash},
+    {"Tilde", sf::Keyboard::Tilde},
+    {"Equal", sf::Keyboard::Equal},
+    {"Hyphen", sf::Keyboard::Hyphen},
+    {"Space", sf::Keyboard::Space},
+    {"Enter", sf::Keyboard::Enter},
+    {"Backspace", sf::Keyboard::Backspace},
+    {"Tab", sf::Keyboard::Tab},
+    {"PageUp", sf::Keyboard::PageUp},
+    {"PageDown", sf::Keyboard::PageDown},
+    {"End", sf::Keyboard::End},
+    {"Home", sf::Keyboard::Home},
+    {"Insert", sf::Keyboard::Insert},
+    {"Delete", sf::Keyboard::Delete},
+    {"Add", sf::Keyboard::Add},
+    {"Subtract", sf::Keyboard::Subtract},
+    {"Multiply", sf::Keyboard::Multiply},
+    {"Divide", sf::Keyboard::Divide},
+    {"Left", sf::Keyboard::Left},
+    {"Right", sf::Keyboard::Right},
+    {"Up", sf::Keyboard::Up},
+    {"Down", sf::Keyboard::Down},
+    {"Numpad0", sf::Keyboard::Numpad0},
+    {"Numpad1", sf::Keyboard::Numpad1},
+    {"Numpad2", sf::Keyboard::Numpad2},
+    {"Numpad3", sf::Keyboard::Numpad3},
+    {"Numpad4", sf::Keyboard::Numpad4},
+    {"Numpad5", sf::Keyboard::Numpad5},
+    {"Numpad6", sf::Keyboard::Numpad6},
+    {"Numpad7", sf::Keyboard::Numpad7},
+    {"Numpad8", sf::Keyboard::Numpad8},
+    {"Numpad9", sf::Keyboard::Numpad9},
+    {"F1", sf::Keyboard::F1},
+    {"F2", sf::Keyboard::F2},
+    {"F3", sf::Keyboard::F3},
+    {"F4", sf::Keyboard::F4},
+    {"F5", sf::Keyboard::F5},
+    {"F6", sf::Keyboard::F6},
+    {"F7", sf::Keyboard::F7},
+    {"F8", sf::Keyboard::F8},
+    {"F9", sf::Keyboard::F9},
+    {"F10", sf::Keyboard::F10},
+    {"F11", sf::Keyboard::F11},
+    {"F12", sf::Keyboard::F12},
+    {"F13", sf::Keyboard::F13},
+    {"F14", sf::Keyboard::F14},
+    {"F15", sf::Keyboard::F15},
+    {"Pause", sf::Keyboard::Pause},
+    {"KeyCount", sf::Keyboard::KeyCount}
+};
+
+}
+
+#endif /* InputNames_h */
